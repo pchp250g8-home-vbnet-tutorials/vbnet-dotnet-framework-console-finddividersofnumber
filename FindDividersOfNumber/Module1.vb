384c535d9406b257d05882ff5a5503aa4c2402a1
Alexander <pchp250g8@tutamail.com>
2026-03-28 12:52:20 +0300

    Fixed Source Code

--- FindDividersOfNumber/Module1.vb
+++ FindDividersOfNumber/Module1.vb
@@ -4,7 +4,7 @@
 		Console.WriteLine("Input an integer positive number")
 		Dim ulNumber As ULong
 		UInt64.TryParse(Console.ReadLine(), ulNumber)
-		Console.WriteLine("The number {0} has the folowing dividers:")
+		Console.WriteLine("The number {0} has the folowing divisors:")
 		For i As ULong = 1 To ulNumber
 			If ulNumber Mod i = 0 Then
 				Console.Write("{0} ", i)
